085e1cbc471e4f74e7c662c4507daaf5bfde3b4d
mingdongtu <1518474913@qq.com>
2020-04-23 20:35:05 +0800

    提交

--- HTTP/demo.js
+++ HTTP/demo.js
@@ -1,1 +1,1 @@
-console.log('i am here')
+console.log('我是demo.js')
--- HTTP/index.js
+++ HTTP/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8081;
 const fs = require('fs');
 const path = require('path');
 
-app.get('/', (req, res) => {
+app.get('/',(req,res) => {
     res.send(`<!DOCTYPE html>
-    <html lang="en"> 
+    <html lang="en">
     <head>
         <title>Document</title>
     </head>
@@ -16,19 +16,18 @@ app.get('/', (req, res) => {
     </body>
     </html>`)
 })
-const moment = require('moment');
-app.get('/demo.js', (req, res) => {
-    console.log('我来了')
-    let jsPath = path.resolve(__dirname, './demo.js');
+const moment = require('moment')
+
+app.get('/demo.js',(req, res)=>{
+    let jsPath = path.resolve(__dirname,'./demo.js');
     let cont = fs.readFileSync(jsPath);
-    res.setHeader('Expires', getGLNZ()) //2分钟
+    // res.setHeader('Expires',getGLNZ())
+    res.setHeader('Cache-Control','public,max-age=120')
     res.end(cont)
 })
-
-function getGLNZ() {
-    return moment().utc().add(2, 'm').format('ddd, DD MMM YYYY HH:mm:ss') + ' GMT';
+function getGLNZ(){
+    return moment().utc().add(2,'m').format('ddd, DD MMM YYYY HH:mm:ss')+' GMT';
 }
-app.listen(port, () => {
-    console.log(__dirname)
-    console.log(`listen on ${port}`)
+app.listen(port,()=>{
+    console.log(`listen on ${port}`)    
 })
