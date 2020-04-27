5cd59a2902a5215dedcb3f9e90836791548c5799
rock-ming <1518474913@qq.com>
2020-04-27 10:47:24 +0800

    数组快速排序算法

--- algorithm/sortArray.js
+++ algorithm/sortArray.js
@@ -1,16 +1,24 @@
 let arr = [109,100,23,2333,33,11,900,2123]
 
-var sortArray = function(arr) {
-      for(let i =0 ;i<arr.length;i++){
-             let idx = i
-            if(idx<arr.length){
-                 let temp;
-                 if(arr[idx]>arr[idx+1]){
-                    temp = arr[idx]
-                    arr[idx] = arr[idx+1];
-                    arr[idx+1] = temp
-                    idx++
-                 }
-            }
+var sortArray = function(arr) { //快速排序：分而治之
+        
+
+      function quickSort(arr){
+            if(arr.length==0) return []
+                let middle = arr[0];
+                let [leftArray,rightArray] = [[],[]]
+                for(let i=0;i<arr.length;i++){
+                    if(arr[i]<middle){
+                         leftArray.push(arr[i])
+                    }
+                    if(arr[i]>middle){
+                        rightArray.push(arr[i])      
+                    }     
+                }
+                console.log(leftArray,middle,rightArray)
+                return quickSort(leftArray).concat(middle,quickSort(rightArray))
       }
+    return quickSort(arr)
+   
 };
+console.log(sortArray(arr))
