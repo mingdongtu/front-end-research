375735aa0a25e5003d42db5f01b6fbf67f44660a
mingdongtu <1518474913@qq.com>
2020-04-27 22:41:17 +0800

    vue 项目

---
+++ vue-mobile/src/components/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="a">不用css modules</div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+
+<style  scoped >
+.a {
+  color: yellow;
+}
+</style>
