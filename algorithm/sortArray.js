ac33cec802491c4c3131d54fbeb2ecc9799aa593
rock-ming <1518474913@qq.com>
2020-04-27 18:54:04 +0800

    排序算法

--- algorithm/sortArray.js
+++ algorithm/sortArray.js
@@ -1,4 +1,4 @@
-let arr = [109,100,23,2333,33,11,900,2123]
+let arr = [109,100,23,2333,23,33,11,900,2123]
 
 var sortArray = function(arr) { //快速排序：分而治之
         
@@ -6,7 +6,8 @@ var sortArray = function(arr) { //快速排序：分而治之
       function quickSort(arr){
             if(arr.length==0) return []
                 let middle = arr[0];
-                let [leftArray,rightArray] = [[],[]]
+                let [leftArray,rightArray,middleArray] = [[],[],[middle]]
+                arr = arr.slice(1)
                 for(let i=0;i<arr.length;i++){
                     if(arr[i]<middle){
                          leftArray.push(arr[i])
@@ -14,11 +15,34 @@ var sortArray = function(arr) { //快速排序：分而治之
                     if(arr[i]>middle){
                         rightArray.push(arr[i])      
                     }     
+                  //   考虑到相等的情况
+                  if(arr[i]==middle){
+                        middleArray.push(arr[i])
+                  }
                 }
                 console.log(leftArray,middle,rightArray)
-                return quickSort(leftArray).concat(middle,quickSort(rightArray))
+                return quickSort(leftArray).concat(middleArray,quickSort(rightArray))
       }
     return quickSort(arr)
    
 };
-console.log(sortArray(arr))
+
+function chooseSort(arr){
+      let circleNum = arr.length ; 
+     for(let i=0;i<circleNum;i++){
+           console.log(circleNum)
+       // 每找到一个相对大数的位置，该位置就不用再次遍历，下面就可以少遍历一个元素
+          for(let j=0;j<circleNum;j++){
+                if(arr[j]>arr[j+1] && j<circleNum-1){
+                     let tmp;
+                     tmp = arr[j]  
+                     arr[j] = arr[j+1];
+                     arr[j+1] = tmp;  
+                }    
+          }
+          console.log('看下内层',circleNum)
+          circleNum --
+     } 
+     return arr      
+}
+console.log(chooseSort(arr))
