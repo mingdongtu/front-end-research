b7de9cd970041a329b9c8481b950cc03ae62bd37
rock-ming <1518474913@qq.com>
2020-05-06 18:08:53 +0800

    vue.config.js 配置问题

--- vue/project/vue-ts/vue.config.js
+++ vue/project/vue-ts/vue.config.js
@@ -1,30 +1,36 @@
 module.exports = {
-    publicPath:'./',
-    outputDir:'build',
-    assetsDir:'asset',
-    indexPath:'app.html',
-    configureWebpack:{},
-    css:{
-          loaderOptions:{
-                less:{  
-                   rules:[
-                         {
-                             test:/\.less$/,
-                             loader:'less-loader'
-                         }
-                   ]    
-                },
-                postcss:{ 
-                   
-                },
-                css:{
-                      rules:[
-                             {
-                                 test:/\.css$/i,
-                                 use:['style-loader','css-loader']
-                             }
-                      ]
-                }
+  publicPath: './',
+  outputDir: 'build',
+  assetsDir: 'asset',
+  indexPath: 'app.html',
+  configureWebpack: {},
+  css: {
+    requireModuleExtension: true,
+    loaderOptions: {
+      less: {
+        rules: [
+          {
+            test: /\.less$/,
+            loader: 'less-loader'
           }
+        ]
+      },
+      // postcss: {
+      //   rules: [
+      //     {
+      //       test: /\.css$/,
+      //       use: [
+      //         'style-loader',
+      //         { loader: 'css-loader', options: { importLoaders: 1, modules: true } },
+      //         { loader: 'postcss-loader', options: { plugins: [] } }
+      //       ]
+      //     }
+      //   ]
+      // }
+      css: {
+           
+      }
+
     }
+  }
 }
