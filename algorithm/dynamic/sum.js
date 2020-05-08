7cd71d530a90c342eee33860c821d2278eaef799
rock-ming <1518474913@qq.com>
2020-05-08 10:00:33 +0800

    动态规划

--- algorithm/dynamic/sum.js
+++ algorithm/dynamic/sum.js
@@ -1,32 +1,25 @@
 //给定一个包含非负数的数组和一个目标整数 k，编写一个函数来判断该数组是否含有连续的子数组，其大小至少为 2，总和为 k 的倍数，
 //即总和为 n*k，其中 n 也是一个整数。
-const nums = [0, 0]
+//动态规划：从大到小 开始推演，找出一个固定模式，除了规模变化，其他没变；
+const nums =  []
+
 var checkSubarraySum = function(nums, k) {
-    k = Math.abs(k)
-    let isHas = false;
-    let sum = 0;
-    const index = nums.indexOf(k)
-    if (nums.length < 2) return false
-    if (index == -1) return false
+        // const ap = new Array(nums.length+1).fill(0)
+       const cpnums = JSON.parse(JSON.stringify(nums))
+       for(let i=0;i<cpnums.length;i++){
 
-    for (let i = 2; i < nums.length + 1; i++) { //子数组长度的可能性
-        //将长度范围为i内的数字组成数组
-        console.log(2)
-        let start = index - (i - 1) < 0 ? 0 : index - i + 1;
-        let end = index + i < nums.length ? index + i - 1 : nums.length - 1;
-        let array = nums.slice(start, end + 1);
-        console.log(start, end, array)
-        for (let j = 0; j < index; j++) { //选取连续i 个数组成的子数组之和
-            let sumArray = array.slice(j, j + i - 1);
-            // console.log(array, sumArray, j, i - 1)
-            sum = sumArray.reduce((a, b) => a + b)
-            if (sum % k == 0) {
-                isHas = true
+           nums = JSON.parse(JSON.stringify(cpnums))
+            for(let j=i;j<nums.length;j++){
+                  if(j-i>0){
+                    nums[j] = (nums[j-1] + nums[j])%k
+                    console.log(nums[j])
+                         if(nums[j]===0) return true
+                         if(nums[j]%k===0) return true
+                  }
             }
-        }
 
-    }
-    return isHas
+       }
+       return false;
 };
-const a = checkSubarraySum(nums, 0)
+const a = checkSubarraySum(nums, 16666677)
 console.log(a)
