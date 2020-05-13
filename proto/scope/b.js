370f1e9465685348d1900e2de5a4804c0d3c8564
rock-ming <1518474913@qq.com>
2020-05-13 13:12:08 +0800

    关于函数声明 和变量声明优先级 以及提升问题研究

--- proto/scope/b.js
+++ proto/scope/b.js
@@ -8,7 +8,7 @@ function testOrder(arg) {
     console.log(a); // 因为函数声明比变量声明优先级高，所以这里a是函数
     var arg = 'hello'; // var arg;变量声明被忽略， arg = 'hello'被执行
     var a = 10; // var a;被忽视; a = 10被执行，a变成number
-    function a() {
+    function a() {  //只是声明了a 是 函数类型
         console.log('fun');
     } // 被提升到作用域顶部
     console.log(a); // 输出10
@@ -17,22 +17,54 @@ function testOrder(arg) {
 console.log('下面是eg1：')
 testOrder('hi');
 
-//eg2: 变量覆盖问题
-
-
- function test2(e){
-    console.log(e); // 999
+//eg2: 形参问题
+ function test1(e){
+    console.log(e); // 999  
     var e = 1000;
     console.log(e) // 1000
 }
 console.log('下面是eg2：')
 test2(999)
+/**
+ 解释：
+ 形参不会被重新定义，只可以被重新赋值
+ */
 
-//eg3:
 
-var f = 10;
+//eg3:变量声明优先级问题
+
+function test2(){
+    console.log(a)
+    var a = 11
+    function a(){  //只是声明了a 是函数类型
+           console.log('我是函数a')
+    }
+}
 console.log('下面是eg3：')
-console.log(f)
+test2()
+/**
+解释：
+1、函数声明的优先级比变量声明的优先级要高
+2、function a(){} 只是声明了a 是函数类型
+ */
 
-var f = 12;
-console.log(f)
+ //eg4:对 函数式声明的深入理解
+
+ function test3(){
+        console.log(b)
+        var b = 100;
+        function b(){
+             console.log('222')
+        }
+        console.log(b)
+ }
+ console.log('下面是eg3：')
+test3()
+
+/**
+ 解释：
+ 1、var 不仅声明了b 这个变量，还给她设置了一个值100；
+ 2、function b(){}只是给b 声明为函数，但是并没有赋值，最终b 还是被var 给赋值为100
+ 3、因为声明可以提升，但是声明的值却不能提升，所以第一个打印是function类型；
+ 4、第二个打印时，b 不仅被声明了，还被赋值了
+ */
