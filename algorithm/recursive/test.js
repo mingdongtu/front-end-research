876528bcd4376f3af81b9e98ab78f4a199758a63
rock-ming <1518474913@qq.com>
2020-06-01 18:01:01 +0800

    提交

--- algorithm/recursive/test.js
+++ algorithm/recursive/test.js
@@ -1,14 +1,14 @@
 
 
-function revise(num){
-   console.log(num)
-    if(num===0) {
-            return 
-    } 
-    for(let i=0 ;i<6;i++){
-        num--;
-        revise(num);
-        console.log('递归之后执行')
+function revise(n){
+    if(n>2) {
+        console.log('我执行了退出',n)
+        return}
+    for(let i=0 ;i<8;i++){
+        n ++ 
+        console.log(i,'>>>>>>>>>>>>>>>>',n)
+       return revise(n);
+        console.log(i,'递_________________执行',n)
     }
 }
-revise(4)
+revise(1)
