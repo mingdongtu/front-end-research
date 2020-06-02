7c1b1d82b6ab0f478e45017ab6a9193cca96b217
rock-ming <1518474913@qq.com>
2020-06-02 09:09:58 +0800

    递归 与 调用栈研究

--- algorithm/recursive/test.js
+++ algorithm/recursive/test.js
@@ -1,14 +1,15 @@
 
 
 function revise(n){
-    if(n>2) {
-        console.log('我执行了退出',n)
+    if(n>4) {
+        // console.log('我执行了退出',n)
         return}
-    for(let i=0 ;i<8;i++){
+    for(let i=0 ;i<1;i++){ //调用自身函数时，剩下没有完成的循环压入栈中
         n ++ 
         console.log(i,'>>>>>>>>>>>>>>>>',n)
-       return revise(n);
+        revise(n); 
+        //递归函数之后的部分会被压入到栈中
         console.log(i,'递_________________执行',n)
     }
 }
-revise(1)
+revise(3)
